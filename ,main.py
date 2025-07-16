4e9963706c55db041f4012b9cda1b36a7c277658
Soham Datta <153073640+HelloPenguin1@users.noreply.github.com>
2025-07-17 01:02:12 +0530

    Fixed file manipulation errors

--- ,main.py
+++ ,main.py
@@ -5,9 +5,10 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import create_retrieval_chain, create_history_aware_retrieval
+from langchain.chains import create_retrieval_chain, create_history_aware_retriever
+from langchain_experimental.text_splitter import SemanticChunker
 from langchain_community.vectorstores import Chroma
-from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_experimental.text_splitter import SemanticChunker
 from langchain_openai.embeddings import OpenAIEmbeddings
@@ -42,65 +43,55 @@ if api_key:
     uploaded_files = st.file_uploader("Choose a pdf file", type = "pdf", accept_multiple_files=False)
 
     if uploaded_files:
-        documents = []
-        for uploaded_file in uploaded_files:
-            ##store in local folder
-            temppdf = f"./temp.pdf"
+        
+        temppdf = f"./temp.pdf"
 
-            with open(temppdf, "wb") as file:
-                file.write(uploaded_file.getvalue())
-                file_name = uploaded_file.name
+        with open(temppdf, "wb") as file:
+                file.write(uploaded_files.getvalue())
+               
+        loader = PyPDFLoader(temppdf)
+        documents = loader.load()
 
-
-
-            loader = PyPDFDirectoryLoader(temppdf)
-            docs = loader.load()
-            documents.extend(docs)
-
-            text_splitter = RecursiveCharacterTextSplitter(chunk_size = 5000, 
-                                                           chunk_overlap = 100)
-            splits = text_splitter.split_documents(documents)
-            vectorstore = Chroma.from_documents(splits, embeddings)
-            retriever = vectorstore.as_retriever()
+        text_splitter = SemanticChunker(OpenAIEmbeddings()) 
+        splits = text_splitter.split_documents(documents)
+        vectorstore = Chroma.from_documents(splits, embeddings)
+        retriever = vectorstore.as_retriever()
 
         #################################################################################################################
 
+        # Fixed reformulation prompt
         reformulation_system_prompt = (
-            "Given a chat history and the latest user question"
+            "Given a chat history and the latest user question "
             "which might reference context in the chat history, "
-            "formulate a standalone question which can be understood"
-            "without the chat histor. DO NOT answer the question"
-            "just reformulate it if needed and otherwise return it as is"
+            "formulate a standalone question which can be understood "
+            "without the chat history. DO NOT answer the question, "
+            "just reformulate it if needed and otherwise return it as is."
         )
 
-        reformulation_prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", reformulation_system_prompt),
-                MessagesPlaceholder("chat_history"),
-                ("human", "{input}")
-            ]
-        )
+        reformulation_prompt = ChatPromptTemplate.from_messages([
+            ("system", reformulation_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}")
+        ])
 
-        history_aware_retriever = create_history_aware_retrieval(llm, retriever, reformulation_prompt)
+        history_aware_retriever = create_history_aware_retriever(llm, retriever, reformulation_prompt)
         
         ###################################################################################################################
 
-        ##Answer question prompt
+        # Fixed answer prompt
         answer_system_prompt = (
-            "You are an assistant for question answering tasks"
-            "Please answer the question as accurately and informatively"
-            "possible using the following retrieved context. In the minimum (unless specified by the question)"
-            "give a three to four line answer to the question."
-            "\n\n {context}"
+            "You are an assistant for question answering tasks. "
+            "Please answer the question as accurately and informatively as "
+            "possible using the following retrieved context. "
+            "Give a concise but complete answer to the question.\n\n"
+            "{context}"
         )
 
-        answer_prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", answer_system_prompt),
-                MessagesPlaceholder("chat_history"),
-                ("human", "{input}")
-            ]
-        )
+        answer_prompt = ChatPromptTemplate.from_messages([
+            ("system", answer_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}")
+        ])
 
         question_answer_chain = create_stuff_documents_chain(llm, answer_prompt)
         rag_pipeline = create_retrieval_chain(history_aware_retriever, question_answer_chain)
@@ -137,15 +128,15 @@ if api_key:
             )
 
             st.write(st.session_state.store)
-            st.success("Assistant:", response['answer'])
+            st.success(f"Assistant: {response['answer']}")
             st.write("Chat History: ", session_history.messages)
 
 else:
     st.warning("Please enter your GROQ API Key")
 
 
-        
-        
+
+
 
 
 
